8391f18ae4b0e81b7046f64a5491396179c55429
Chao Wang <cwang8023@gmail.com>
2017-01-03 00:07:16 +0800

    add timezone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Tower
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Beijing'
 
     config.generators.javascript_engine = :js
     config.generators.test_framework = :rspec
