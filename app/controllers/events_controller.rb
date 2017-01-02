f52c67cb2d116e5f50606c26be3425ad1647f419
Chao Wang <cwang8023@gmail.com>
2017-01-02 23:08:02 +0800

    include initiator in events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,6 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @events = Event.by_projects(current_user.projects_by_team_id(params[:team_id])).recent.includes(:target, :source)
+    @events = Event.by_projects(current_user.projects_by_team_id(params[:team_id])).recent.includes(:target, :source, :initiator)
   end
 end
