4dd8ab9eafa20b553a3672ef329ed05ca446009e
Chao Wang <cwang8023@gmail.com>
2017-01-02 21:56:44 +0800

    style index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.find_or_create_by(email: 'user1@example.com') do |u|
+user = User.find_or_create_by(email: 'user1@example.com') do |u|
   u.name = '用户1'
+  u.password = '1234567890'
+  u.password_confirmation = '1234567890'
+end
+
+user2 = User.find_or_create_by(email: 'user2@example.com') do |u|
+  u.name = '用户2'
+  u.password = '1234567890'
+  u.password_confirmation = '1234567890'
+end
+
+user3 = User.find_or_create_by(email: 'user3@example.com') do |u|
+  u.name = '用户3'
   u.password = '1234567890'
   u.password_confirmation = '1234567890'
 end
@@ -14,3 +26,10 @@ end
 Team.find_or_create_by(name: 'Team-1') do |team|
   team.team_members << TeamMember.create(user: User.first)
 end
+
+Project.find_or_create_by(name: 'Project-1') do |project|
+  project.team = Team.first
+  project.project_members << ProjectMember.create(user: User.first)
+end
+
+Todo.find_or_create_by(project: Project.first, author: user, name: 'First todo')
