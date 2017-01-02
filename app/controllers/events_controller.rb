312d63abc854c5bc29a0f5a4cd27fcf268543aac
Chao Wang <cwang8023@gmail.com>
2017-01-02 18:24:09 +0800

    add todo

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # Todo: should limit projects in specified team
     @events = Event.by_projects(current_user.projects).recent.includes(:target, :source)
   end
 end
